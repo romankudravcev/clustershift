62d147ba234add1e6f5d68a334713c204d6dd16b
Roman Kudravcev <roman.kudravcev@lion5.io>
2024-11-06 20:36:39 +0100

    add k8s client operations

---
+++ internal/kube/types.go
@@ -0,0 +1,25 @@
+package kube
+
+type ResourceType interface {
+	IsResourceType()
+}
+
+type K8sResourceType string
+type TraefikResourceType string
+
+const (
+	Deployment      K8sResourceType     = "Deployment"
+	ConfigMap       K8sResourceType     = "ConfigMap"
+	Ingress         K8sResourceType     = "Ingress"
+	Secret          K8sResourceType     = "Secret"
+	Namespace       K8sResourceType     = "Namespace"
+	Service         K8sResourceType     = "Service"
+	Middleware      TraefikResourceType = "Middleware"
+	IngressRoute    TraefikResourceType = "IngressRoute"
+	IngressRouteTCP TraefikResourceType = "IngressRouteTCP"
+	IngressRouteUDP TraefikResourceType = "IngressRouteUDP"
+	TraefikService  TraefikResourceType = "TraefikService"
+)
+
+func (K8sResourceType) IsResourceType()     {}
+func (TraefikResourceType) IsResourceType() {}
