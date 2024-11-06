8b9fc100651afbd8442ef7852c4dab8e9c71d49e
Roman Kudravcev <roman.kudravcev@lion5.io>
2024-11-06 23:51:37 +0100

    add rest of submariner

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,11 +1,14 @@
 package constants
 
 const (
+	// Submariner constants
 	SubmarinerRepoName          = "submariner-latest"
 	SubmarinerRepoURL           = "https://submariner-io.github.io/submariner-charts/charts"
 	SubmarinerBrokerChartName   = "submariner-latest/submariner-k8s-broker"
 	SubmarinerOperatorChartName = "submariner-latest/submariner-operator"
 	SubmarinerBrokerNamespace   = "submariner-k8s-broker"
 	SubmarinerOperatorNamespace = "submariner-operator"
-	Debug                       = true
+	SubmarinerBrokerClientToken = "submariner-k8s-broker-client-token"
+	// Debug flag helm
+	Debug = true
 )
