688135b4ab6d064a1c734eb11591bf1b36476489
Roman Kudravcev <romankudravcev1@gmail.com>
2024-11-11 22:10:23 +0100

    add connectivity probe

---
+++ cmd/clustershift/diagnose.go
@@ -0,0 +1,31 @@
+package clustershift
+
+import (
+	"clustershift/pkg/connectivity"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	cluster1 string
+	cluster2 string
+
+	diagnose = &cobra.Command{
+		Use:   "diagnose",
+		Short: "diagnose connectivity between two clusters",
+		Run: func(cmd *cobra.Command, args []string) {
+			connectivity.RunClusterConnectivityProbe(cluster1, cluster2)
+		},
+	}
+)
+
+func init() {
+	diagnose.Flags().StringVarP(&cluster1, "origin", "o", "", "Specify the path of the kubeconfig for the origin cluster")
+	diagnose.Flags().StringVarP(&cluster2, "target", "t", "", "Specify the path of the kubeconfig for the target cluster")
+
+	// Mark flags as required
+	diagnose.MarkFlagRequired("origin")
+	diagnose.MarkFlagRequired("target")
+
+	rootCmd.AddCommand(diagnose)
+}
