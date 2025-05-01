1da6e4003bbd0383033f21288ce996b88146408e
Roman Kudravcev <romankudravcev1@gmail.com>
2025-05-01 14:37:58 +0200

    chore: add skupper constants

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -45,6 +45,9 @@ const (
 	LinkerdNamespace                 = "linkerd"
 	LinkerdMultiClusterNamespace     = "linkerd-multicluster"
 
+	// Skupper constants
+	SkupperSiteControllerURL = "https://raw.githubusercontent.com/skupperproject/skupper/refs/heads/1.8/cmd/site-controller/deploy-watch-all-ns.yaml"
+
 	// CNPG constants
 	CNPGOperatorURL   = "https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.24/releases/cnpg-1.24.1.yaml"
 	CNPGNamespace     = "cnpg-system"
